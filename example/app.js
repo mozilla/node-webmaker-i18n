a125760a9a1ca1d4d8ec35090a944abe7358a91d
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2013-08-19 15:47:31 -0400

    Add sample app in example/

---
+++ example/app.js
@@ -0,0 +1,32 @@
+var express = require('express'),
+    i18n = require('../'),
+    nunjucks = require('nunjucks'),
+    path = require('path');
+
+var app = express(),
+    nunjucksEnv = new nunjucks.Environment( new nunjucks.FileSystemLoader( path.join( __dirname, 'views' )), {
+      autoescape: true
+    });
+
+nunjucksEnv.addFilter("instantiate", function(input) {
+    var tmpl = new nunjucks.Template(input);
+    return tmpl.render(this.getVariables());
+});
+nunjucksEnv.express( app );
+
+// Setup locales with i18n
+app.use( i18n.middleware({
+  supported_languages: [
+    'en-US'
+  ],
+  default_lang: 'en-US',
+  translation_directory: path.join( __dirname, 'locale' )
+}));
+
+app.get( "/", function( req, res ) {
+  res.render( "index.html" );
+});
+
+app.listen(8000, function() {
+  console.log("Server listening ( http://localhost:8000");
+});
