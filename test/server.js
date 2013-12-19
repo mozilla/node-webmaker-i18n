92fa72402a471cc8753eda600e8d74bdc5f20a41
Ali Al Dallal <ali@alicoding.com>
2013-12-19 18:50:59 -0500

    NOBUG - fix indentation for test.js

--- test/server.js
+++ test/server.js
@@ -1,21 +1,19 @@
 var express = require('express');
 var app = express(),
-		httpServer;
+    httpServer;
 
-module.exports = function(i18n) {
-	app.get( "/strings/:lang?", i18n.stringsRoute() );
+module.exports = function (i18n) {
+  app.get("/strings/:lang?", i18n.stringsRoute());
 
-	return {
-		listen: function (PORT, done) {
-			httpServer = require('http').createServer(app);
-			httpServer.listen(PORT);
-			done();
-		},
-		close: function(done) {
-			httpServer.close();
-			done();
-		}
-	}
+  return {
+    listen: function (PORT, done) {
+      httpServer = require('http').createServer(app);
+      httpServer.listen(PORT);
+      done();
+    },
+    close: function (done) {
+      httpServer.close();
+      done();
+    }
+  }
 }
-
-
