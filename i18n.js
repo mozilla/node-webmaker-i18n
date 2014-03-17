0863116a6effcf7a5454c8fe8caf915acc66d399
Ali Al Dallal <ali@alicoding.com>
2014-03-17 14:58:57 -0400

    Add localeInfo.langPrefs

--- i18n.js
+++ i18n.js
@@ -441,6 +441,25 @@ exports.middleware = function(options) {
     return options.mappings[lang] || lang;
   }
 
+  // Given  [ { lang: 'th', quality: 1 }, { lang: 'en', quality: 0.8 }, { lang: 'es', quality: 0.6 } ]
+  // We will remove first element and return next preferred languages in array format ['en', 'es']
+  function getOtherLangPrefs(languages) {
+    return languages.slice(1).map(function(item) { return item.lang; });
+  }
+
+  // Given user's otherLangPrefs ['th', 'en-CA', 'es', 'fr', 'ar'] we compare with our
+  // currentSupportLang ['en-US', 'en-CA', 'th'] and return match found.
+  // ['th', 'en-CA']
+  function getAlternateLangSupport(otherLangPrefs, currentSupportLang) {
+    var support = [];
+    otherLangPrefs.forEach(function(lang) {
+      if(currentSupportLang.indexOf(lang) !== -1) {
+        support.push(lang);
+      }
+    });
+    return support;
+  }
+
   return function(req, resp, next) {
     checkUrlLocale(req);
 
@@ -467,6 +486,8 @@ exports.middleware = function(options) {
     localeInfo.locale = locale;
     localeInfo.momentLang = langToMomentJSLang(lang);
     localeInfo.direction = lang_dir;
+    localeInfo.otherLangPrefs = getOtherLangPrefs(langs);
+    localeInfo.alternateLangs = getAlternateLangSupport(localeInfo.langPrefs, listOfLanguages)
 
     locals.localeInfo = localeInfo;
     req.localeInfo = localeInfo;
